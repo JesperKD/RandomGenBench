ce9ef98567f50481fe2d470d1ba8507179355aa1
Jesper Klejs Deleurand (JESP446C) <jesperd55@gmail.com>
2021-01-04 10:22:17 +0100

    Initial commit

---
+++ RandomGenBench/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomGenBench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tilfældig tal generering:");
+            Console.WriteLine();
+            Randomer.RNGRandom();
+            Randomer.SysRandom();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RandomGenBench/Randomer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomGenBench
+{
+    class Randomer
+    {
+        public static void RNGRandom()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] data = new byte[4];
+
+                for (int i = 0; i < 100; i++)
+                {
+                    rng.GetBytes(data);
+
+                    int value = BitConverter.ToInt32(data, 0);
+                    Console.Write(value + " ");
+                }
+                stopwatch.Stop();
+                Console.WriteLine();
+                Console.WriteLine("Time Spent:" + stopwatch.ElapsedMilliseconds);
+                Console.WriteLine();
+            }
+
+
+        }
+
+        public static void SysRandom()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            Random random = new Random();
+            stopwatch.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write(random.Next(100000) + " ");
+            }
+            stopwatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Time Spent:" + stopwatch.ElapsedMilliseconds);
+            Console.WriteLine();
+        }
+    }
+}
